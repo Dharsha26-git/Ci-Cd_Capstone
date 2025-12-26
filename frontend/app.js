d51272138ff22edf41296f2ace44e8113ec3951c
Dharsha <vec.194n1a0528@gmail.com>
2025-12-26 12:14:40 +0530

    Add simple frontend and integrate with backend

---
+++ frontend/app.js
@@ -0,0 +1,10 @@
+function loadData() {
+  fetch("http://localhost:5000/")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("result").innerText = data.message;
+    })
+    .catch(error => {
+      document.getElementById("result").innerText = "Backend not reachable";
+    });
+}
