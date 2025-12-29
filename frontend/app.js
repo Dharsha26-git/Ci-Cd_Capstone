7c675afa09a26aa42e7139d9eb3fdcb959b1a701
Dharsha <vec.194n1a0528@gmail.com>
2025-12-29 18:08:37 +0530

    Added and Modified

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 function loadData() {
-  fetch("http://localhost:5000/")
+  fetch("http://backend:5000/")
     .then(response => response.json())
     .then(data => {
       document.getElementById("result").innerText = data.message;
