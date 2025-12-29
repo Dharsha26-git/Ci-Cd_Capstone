efeae221d6892cc244b341e9f339c9cf32302a52
Dharsha <vec.194n1a0528@gmail.com>
2025-12-29 14:34:36 +0530

    week - 1 complete: stabilization and cleanup

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
+
+
+
 def get_db_connection():
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST", "db"),
@@ -41,17 +45,23 @@ def init_db():
 
 @app.route("/data")
 def data():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO test_data (name) VALUES ('DevOps');")
-    conn.commit()
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
 
-    cur.execute("SELECT * FROM test_data;")
-    rows = cur.fetchall()
+        cur.execute("INSERT INTO test_data (name) VALUES 	('DevOps');")
+        conn.commit()
 
-    cur.close()
-    conn.close()
-    return jsonify(rows)
+        cur.execute("SELECT * FROM test_data;")
+        rows = cur.fetchall()
+
+        cur.close()
+        conn.close()
+        return jsonify(rows)
+
+    except Exception as e:
+        return jsonify({"error": "Database not reachable"}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
